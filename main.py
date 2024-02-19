2f4aa98e9f85cce81868f069216a538b09b417a7
Viktor Synek <106328886+viktorsynek@users.noreply.github.com>
2024-02-19 16:23:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
+############################################   -== INFORMATION ==-   ############################################
+
+#### THE PROGRAM WAS CREATED AND PUBLISHED BY:
+#### https://github.com/viktorsynek
+#### https://www.linkedin.com/in/viktor-synek/
+
+#############################################   -== PROGRAM ==-   ###############################################
+
+#IMPORT LIBRARIES
 import unittest
 
 def vowel_count(string):
+    # ALL THE VOWELS IN ONE LIST
     vowels = ['a', 'e', 'i', 'o', 'u']
     count = 0
+    # LOOP THROUGH THE GIVEN STRING
     for i in string:
+        # IF THE CHARACTER IS INSIDE THE VOWELS LIST 
+        # MEANING THE CHARACTER IS A VOWEL
         if i in vowels:
+            # ADD ONE TO THE VOWEL COUNTER
             count+= 1
     
     return count
